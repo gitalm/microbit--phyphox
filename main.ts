84f2a4dd8ff13560b83437971e537f23345438a7
gitalm <johannes.almer@web.de>
2020-11-26 09:31:06 +0100

    blocks und script löschen

--- main.ts
+++ main.ts
@@ -1,10 +1,1 @@
-bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.Yes)
-})
-bluetooth.onBluetoothDisconnected(function () {
-    basic.showIcon(IconNames.No)
-})
-bluetooth.startUartService()
-basic.forever(function () {
-    bluetooth.uartWriteLine("" + input.rotation(Rotation.Pitch) + ";" + pins.analogReadPin(AnalogPin.P0) + ";")
-})
+// leave empty
